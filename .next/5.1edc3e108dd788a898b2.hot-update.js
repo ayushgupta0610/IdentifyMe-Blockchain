9c4184974e10f3e288dc24e3cebf7794351bbcc5
ayush <ayushgupta0610@gmail.com>
2018-05-06 20:40:44 +0530

    Routing changes

---
+++ .next/5.1edc3e108dd788a898b2.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate(5,{
+
+/***/ 656:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var routes = __webpack_require__(970)();
+
+routes.add('/MemberPortal', '/MemberPortal').add('/AgencyPortal', '/AgencyPortal');
+
+module.exports = routes;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNLLEFBREwsSUFDUyxBQURULGlCQUMwQixBQUQxQixpQkFFSyxBQUZMLElBRVMsQUFGVCxpQkFFMEIsQUFGMUI7O0FBSUEsT0FBTyxBQUFQLFVBQWlCLEFBQWpCIiwiZmlsZSI6InJvdXRlcy5qcyIsInNvdXJjZVJvb3QiOiJFOi9Db29sIFN0dWZmL1Byb2plY3RzL0FpY3VtZW4tVGFzayJ9
+
+ ;(function register() { /* react-hot-loader/webpack */ if (true) { if (typeof __REACT_HOT_LOADER__ === 'undefined') { return; } /* eslint-disable camelcase, no-undef */ var webpackExports = typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__ : module.exports; /* eslint-enable camelcase, no-undef */ if (typeof webpackExports === 'function') { __REACT_HOT_LOADER__.register(webpackExports, 'module.exports', "E:\\Cool Stuff\\Projects\\Aicumen-Task\\routes.js"); return; } /* eslint-disable no-restricted-syntax */ for (var key in webpackExports) { /* eslint-enable no-restricted-syntax */ if (!Object.prototype.hasOwnProperty.call(webpackExports, key)) { continue; } var namedExport = void 0; try { namedExport = webpackExports[key]; } catch (err) { continue; } __REACT_HOT_LOADER__.register(namedExport, key, "E:\\Cool Stuff\\Projects\\Aicumen-Task\\routes.js"); } } })();
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNS4xZWRjM2UxMDhkZDc4OGE4OThiMi5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcm91dGVzLmpzPzEwZDBmYjgiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3Qgcm91dGVzID0gcmVxdWlyZSgnbmV4dC1yb3V0ZXMnKSgpO1xyXG5cclxucm91dGVzXHJcbiAgICAuYWRkKCcvTWVtYmVyUG9ydGFsJywgJy9NZW1iZXJQb3J0YWwnKVxyXG4gICAgLmFkZCgnL0FnZW5jeVBvcnRhbCcsICcvQWdlbmN5UG9ydGFsJyk7XHJcblxyXG5tb2R1bGUuZXhwb3J0cyA9IHJvdXRlcztcclxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vcm91dGVzLmpzIl0sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBR0E7Ozs7Ozs7QSIsInNvdXJjZVJvb3QiOiIifQ==
--- routes.js
+++ routes.js
@@ -1,4 +1,7 @@
 const routes = require('next-routes')();
 
+routes
+    .add('/MemberPortal', '/MemberPortal')
+    .add('/AgencyPortal', '/AgencyPortal');
 
 module.exports = routes;
