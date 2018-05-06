169e7322a246b5769b050c61106cbc706e62ebd3
ayush <ayushgupta0610@gmail.com>
2018-05-06 20:54:58 +0530

    Next folder removed

--- .next/dist/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/MemberPortal', '/MemberPortal').add('/AgencyPortal', '/AgencyPortal');
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNLLEFBREwsSUFDUyxBQURULGlCQUMwQixBQUQxQixpQkFFSyxBQUZMLElBRVMsQUFGVCxpQkFFMEIsQUFGMUI7O0FBSUEsT0FBTyxBQUFQLFVBQWlCLEFBQWpCIiwiZmlsZSI6InJvdXRlcy5qcyIsInNvdXJjZVJvb3QiOiJFOi9Db29sIFN0dWZmL1Byb2plY3RzL0FpY3VtZW4tVGFzayJ9
